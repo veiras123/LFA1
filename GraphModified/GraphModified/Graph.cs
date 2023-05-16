e294c30f151da7d31395d79e26fc377852b45d93
veiras123 <120336723+veiras123@users.noreply.github.com>
2023-05-16 17:40:16 +0300

    Add files via upload

---
+++ GraphModified/GraphModified/Graph.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GraphModified
+{
+    class Graph
+    {
+        public ObservableCollection<Node> Nodes { get; private set; }
+        public ObservableCollection<Edge> Edges { get; private set; }
+
+        private int nextIndex;
+
+
+        public Graph()
+        {
+            this.Nodes = new ObservableCollection<Node>();
+            this.Edges = new ObservableCollection<Edge>();
+            this.nextIndex = 1;
+        }
+
+        public Node AddNode(double x, double y)
+        {
+            Node node = new Node(nextIndex, x, y);
+            this.Nodes.Add(node);
+            nextIndex++;
+            return node;
+        }
+
+        public void AddEdge(Node startNode, Node endNode)
+        {
+            this.Edges.Add(new Edge(startNode, endNode));
+        }
+    }
+    public class Edge : DependencyObject
+    {
+
+
+        public Node StartNode
+        {
+            get { return (Node)GetValue(StartNodeProperty); }
+            set { SetValue(StartNodeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartNode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartNodeProperty =
+            DependencyProperty.Register("StartNode", typeof(Node), typeof(Edge), new PropertyMetadata(null));
+
+
+
+        public Node EndNode
+        {
+            get { return (Node)GetValue(EndNodeProperty); }
+            set { SetValue(EndNodeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EndNode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EndNodeProperty =
+            DependencyProperty.Register("EndNode", typeof(Node), typeof(Edge), new PropertyMetadata(null));
+
+        public Edge(Node startNode, Node endNode)
+        {
+            this.StartNode = startNode;
+            this.EndNode = endNode;
+        }
+    }
+
+    public class Node : DependencyObject
+    {
+
+
+        public int Index
+        {
+            get { return (int)GetValue(IndexProperty); }
+            set { SetValue(IndexProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Index.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IndexProperty =
+            DependencyProperty.Register("Index", typeof(int), typeof(Node), new PropertyMetadata(0));
+
+
+
+        public double X
+        {
+            get { return (double)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for X.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register("X", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+
+
+
+        public double Y
+        {
+            get { return (double)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Y.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register("Y", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+
+
+        public double CenterX
+        {
+            get { return (double)GetValue(CenterXProperty); }
+            set { SetValue(CenterXProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CenterX.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CenterXProperty =
+            DependencyProperty.Register("CenterX", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+
+
+
+        public double CenterY
+        {
+            get { return (double)GetValue(CenterYProperty); }
+            set { SetValue(CenterYProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CenterY.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CenterYProperty =
+            DependencyProperty.Register("CenterY", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+
+
+
+        public double Size
+        {
+            get { return (double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Size.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register("Size", typeof(double), typeof(Node), new PropertyMetadata(0.0));
+
+
+
+
+
+        public Node(int index, double centerX, double centerY)
+        {
+            this.Index = index;
+            this.CenterX = centerX;
+            this.CenterY = centerY;
+            this.Size = 50;
+            this.X = this.CenterX - this.Size / 2;
+            this.Y = this.CenterY - this.Size / 2;
+        }
+    }
+}
---
+++ GraphModified/GraphModified/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphModified
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Graph graph;
+        private Canvas canvasNodes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            graph = new Graph();
+
+            ListBoxNodes.ItemsSource = graph.Nodes;
+            listBoxEdges.ItemsSource = graph.Edges;
+
+        }
+
+        private void CanvasNodes_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+
+                if (canvasNodes != null)
+                {
+                    Point point = e.GetPosition(canvasNodes);
+                    graph.AddNode(point.X, point.Y);
+                }
+            }
+            catch
+            {
+                //not used
+            }
+        }
+
+        private bool isDraggingNode = false;
+        private Point? previousPoint = null;
+        private Node firstNode = null;
+
+        private void GridNode_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+
+            if (grid != null)
+            {
+                grid.CaptureMouse();
+
+
+                if (radioAddNode.IsChecked == true)
+                {
+                    isDraggingNode = true;
+                    previousPoint = e.GetPosition(canvasNodes);
+                }
+                if (radioAddEdge.IsChecked == true)
+                {
+                    Node currentNode = null;
+                    currentNode = grid.DataContext as Node;
+
+                    if (firstNode == null)
+                    {
+                        firstNode = currentNode;
+                    }
+                    else
+                    {
+                        graph.AddEdge(firstNode, currentNode);
+                        firstNode = null;
+                    }
+                }
+
+
+                if (radioDelete.IsChecked == true)
+                {
+                   
+                    Point currentPoint = e.GetPosition(canvasNodes);
+                    
+                    HitTestResult result = VisualTreeHelper.HitTest(canvasNodes, currentPoint);
+                    if (result != null)
+                    {
+                        canvasNodes.Children.Remove(result.VisualHit as UIElement);
+                    }
+                }
+
+
+            }
+        }
+
+        private void GridNode_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingNode)
+            {
+                Grid element = sender as Grid;
+                if (element != null)
+                {
+                    Node node = element.DataContext as Node;
+                    if (node != null && previousPoint.HasValue)
+                    {
+                        Point point = e.GetPosition(canvasNodes);
+
+                        double xDiff = point.X - previousPoint.Value.X;
+                        double yDiff = point.Y - previousPoint.Value.Y;
+
+                        node.X += xDiff;
+                        node.Y += yDiff;
+                        node.CenterX += xDiff;
+                        node.CenterY += yDiff;
+
+                        previousPoint = point;
+                    }
+                }
+            }
+        }
+
+        private void GridNode_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            if (grid != null)
+            {
+                grid.ReleaseMouseCapture();
+                isDraggingNode = false;
+                previousPoint = null;
+            }
+        }
+
+        private void CanvasNodes_Loaded(object sender, RoutedEventArgs e)
+        {
+            canvasNodes = sender as Canvas;
+        }
+    }
+}
